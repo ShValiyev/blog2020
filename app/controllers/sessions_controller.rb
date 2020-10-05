39d68fe0b55afc9b49dfa2532dc4726f1de126b2
Valiev Sherzod <valiev0208@gmail.com>
2020-10-05 18:27:20 +0300

    Test

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to say_hello_path
+    else
+      redirect_to login_path, notice: 'Invalid username or password'
+    end
+  end
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path, notice: 'Logged out'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new', as: :login
   resources :users
   resources :articles
   get 'say/hello'
   get 'say/goodbye'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'say#hello'
+  post 'sessions/create', as: :create_session
+  get 'sessions/destroy', as: :destroy_session
 end
